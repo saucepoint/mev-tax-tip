ee14b8c74281051dc26aabca5a207641258bab23
saucepoint <saucepoint@protonmail.com>
2025-03-22 02:54:50 -0400

    prep deployment

--- script/00_Counter.s.sol
+++ script/00_Counter.s.sol
@@ -13,21 +13,18 @@ import {HookMiner} from "v4-periphery/src/utils/HookMiner.sol";
 contract MEVTaxTestInProdScript is Script, Constants {
     function setUp() public {}
 
-    function run() public {
+    function run(address _owner) public {
         // hook contracts must have specific flags encoded in the address
-        uint160 flags = uint160(
-            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
-                | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
-        );
+        uint160 flags = uint160(Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG);
 
         // Mine a salt that will produce a hook address with the correct flags
-        bytes memory constructorArgs = abi.encode(POOLMANAGER);
+        bytes memory constructorArgs = abi.encode(POOLMANAGER, _owner);
         (address hookAddress, bytes32 salt) =
             HookMiner.find(CREATE2_DEPLOYER, flags, type(MEVTaxTestInProd).creationCode, constructorArgs);
 
         // Deploy the hook using CREATE2
         vm.broadcast();
-        MEVTaxTestInProd counter = new MEVTaxTestInProd{salt: salt}(IPoolManager(POOLMANAGER), address(this));
+        MEVTaxTestInProd counter = new MEVTaxTestInProd{salt: salt}(IPoolManager(POOLMANAGER), _owner);
         require(address(counter) == hookAddress, "MEVTaxTestInProdScript: hook address mismatch");
     }
 }
